48494393e53d776e5294874a55ab5799fa652e1c
Shourav Kundu <shouravkundu19@gmail.com>
2024-10-02 20:50:24 +0600

    add files update 3

---
+++ 3. Print some text/1 execution 3,1.c
@@ -0,0 +1,12 @@
+/* print some text */
+//header file adding - lower-case letters
+#include <stdio.h>
+
+//main function
+int main(void)
+{
+    //printf function statement
+    printf("this is a string\n");
+    
+    return 0;
+}
---
+++ 3. Print some text/2 execution 3,2.c
@@ -0,0 +1,12 @@
+/* print some text */
+//header file adding - upper-case letters
+#include <STDIO.H>
+
+//main function
+int main(void)
+{
+    //printf function statement
+    printf("this is a string\n");
+    
+    return 0;
+}
