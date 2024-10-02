4a4e4804826f4db31498425c243d9e59a3b24355
Shourav Kundu <shouravkundu19@gmail.com>
2024-10-03 00:55:13 +0600

    add files update 5

---
+++ 5. Value assign in variable/1 execution 5,1.c
@@ -0,0 +1,15 @@
+/*assign value to declared variables*/
+#include <stdio.h>
+
+int main()
+{
+    // variable declaration and value assign
+    int a, b, c;
+    a = 1; b = 2; c = 406;
+    float fnum;
+    fnum = 203.35235;
+    char c1, c2, c3 = 'D';
+    c1 = 'd'; c2 = '3';
+
+    return 0;
+}
